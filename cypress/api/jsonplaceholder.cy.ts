82c788a5ebb1213c27ebe59f5c127468d537f79c
Kiryl Arlou <Kiryl.Arlou@itechart-group.com>
2023-03-09 18:40:19 +0300

    api tests have been moved

--- cypress/api/jsonplaceholder.cy.ts
+++ cypress/api/jsonplaceholder.cy.ts
@@ -1,14 +1,19 @@
-describe('Api testing', () => {
+describe('jsonplaceholder api testing', () => {
+
+    beforeEach(() => {
+        cy.visit('https://swapi.dev/api')
+    })
 
     describe('GET', () => {
+        
         it('Get user with id 1 list', () => {
-            cy.request('GET', 'https://swapi.dev/api/people/1').then((response) => {
+            cy.request('GET', '/people/1').then((response) => {
                 expect(response).to.have.property('status', 200)
                 expect(response.body).to.have.property('name', 'Luke Skywalker')
             })
         })
         it('Get users list', () => {
-            cy.request('GET', 'https://swapi.dev/api/people').then((response) => {
+            cy.request('GET', '/people').then((response) => {
                 console.log(response.body.length)
                 expect(response.body.results).to.have.length(10)
             })
