113526f9e00e9cf8696cd8b50b1d79aea004ccab
Kiryl Arlou <Kiryl.Arlou@itechart-group.com>
2023-03-09 19:04:45 +0300

    config changes

--- cypress/cypress.config.ts
+++ cypress/cypress.config.ts
@@ -5,7 +5,7 @@ import { logger } from './support/logger'
 
 export default defineConfig({
   e2e: {
-    specPattern: 'cypress/**/*.cy.ts',
+    specPattern: 'cypress/e2e/*.cy.ts',
     baseUrl: BASE_URL,
     defaultCommandTimeout: DEFAULT_WAITNG_TIME,
     supportFile: 'cypress/support/index.ts',
